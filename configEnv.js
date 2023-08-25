926f566d268649756968e07e947f005b39acc3a3
Art Frich <frich.g23@yandex.ru>
2023-08-25 10:43:30 +0500

    refactor: change bd name by default

--- configEnv.js
+++ configEnv.js
@@ -1,4 +1,4 @@
 const { NODE_ENV } = process.env;
 module.exports.PORT = NODE_ENV === 'production' ? process.env.PORT : 3002;
 module.exports.JWT_SECRET = NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev-secret';
-module.exports.MONGO_URI = NODE_ENV === 'production' ? process.env.MONGO_URI : 'mongodb://localhost:27017/mydb';
+module.exports.MONGO_URI = NODE_ENV === 'production' ? process.env.MONGO_URI : 'mongodb://localhost:27017/bitfilmsdb';
